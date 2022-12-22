7060f9aa371acb59559f434b23cdc5cf628adf71
Gauri Wadekar <77197943+GauriW24@users.noreply.github.com>
2022-12-22 21:11:38 +0530

    Create TestMain.java

---
+++ TestMain.java
@@ -0,0 +1,20 @@
+public class TestMain {
+   public static void main(String[] args) {
+      // Test constructor and toString()
+      InvoiceItem inv1 = new InvoiceItem(101, "blue ballons", 800, 0.05);
+      System.out.println(inv1);  // toString();
+
+      // Test Setters and Getters
+      inv1.setQty(999);
+      inv1.setUnitPrice(0.99);
+      System.out.println(inv1);  // toString();
+      System.out.println("\n\nid is: " + inv1.getId());
+      System.out.println("desc is: " + inv1.getDesc());
+      System.out.println("qty is: " + inv1.getQty());
+      System.out.println("unitPrice is: " + inv1.getUnitPrice());
+
+      // Test getTotal()
+      System.out.println("\n-----------------------------\n");
+      System.out.println("The total is: " + inv1.getTotal());
+   }
+}
